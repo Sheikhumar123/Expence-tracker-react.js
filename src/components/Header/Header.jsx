cd2521cfbed0c42f69a9e0ec599ab8078b6ec1a6
Sheikhumar123 <xhk.umar5091@gmail.com>
2021-04-29 23:11:01 +0500

    full final

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import './Header.css';
 function Header() {
     return (
         <div className="header">
-            <h2>Expense Tracker By Shahbaz Ali</h2>
+            <h2>Expense Tracker By "Sheikh Umar "</h2>
         </div>
     );
 }
